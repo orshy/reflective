1bff08fcc6747edfbee336477b4e15fb5dcc839c
Philip Orchard <philiporchard@gmail.com>
2018-01-28 02:36:36 -0800

    add assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,40 @@ import Helmet from 'react-helmet'
 import pic01 from '../images/pic01.jpg'
 
 class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    render() {
+        const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+        const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
-    return (
-      <div>
-        <Helmet>
-            <title>{get(this, 'props.data.site.siteMetadata.title')}</title>
-            <meta name="description" content={get(this, 'props.data.site.siteMetadata.description')} />
-        </Helmet>
+        return ( <
+            div >
+            <
+            Helmet >
+            <
+            title > {get(this, 'props.data.site.siteMetadata.title') } < /title> <
+            meta name = "description"
+            content = {get(this, 'props.data.site.siteMetadata.description') }
+            /> <
+            /Helmet> <
+            video autoPlay loop id = "bg-video"
+            poster = "../images/tree-mobile.jpg.jpg" >
+            <
+            source src = "https://s3-us-west-2.amazonaws.com/kameronjohnson/treetop.mp4"
+            type = "video/mp4" / > Your browser does not support the video tag.I suggest you upgrade your browser. <
+            /video>
 
-      </div>
-    )
-  }
+            <
+            /div>
+        )
+    }
 }
 
 BlogIndex.propTypes = {
-  route: React.PropTypes.object,
+    route: React.PropTypes.object,
 }
 
 export default BlogIndex
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query IndexQuery {
     site {
       siteMetadata {
