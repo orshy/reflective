bb11ada8621e8cea70d3473b08cd38e3686283da
Philip Orchard <philiporchard@gmail.com>
2018-02-04 21:11:51 -0800

    update layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,44 @@
 import React from 'react'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-btc"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Reflective Ventures</h1>
-                <p>Tactful Innovation with Impact</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Info</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+const Header = (props) => ( <
+    header id = "header"
+    style = { props.timeout ? { display: 'none' } : {} } >
+    <
+    div className = "logo" >
+    <
+    span className = "icon fa-btc" > < /span> <
+    /div> <
+    div className = "content" >
+    <
+    div className = "inner" >
+    <
+    h1 > Reflective Ventures < /h1> <
+    p > Tactful Innovation with Impact < /p> <
+    /div> <
+    /div> <
+    nav >
+    <
+    ul >
+    <
+    li > < a href = "javascript:;"
+    onClick = {
+        () => { props.onOpenArticle('intro') } } > Blog < /a></li >
+    <
+    li > < a href = "javascript:;"
+    onClick = {
+        () => { props.onOpenArticle('work') } } > Companies < /a></li >
+    <
+    li > < a href = "javascript:;"
+    onClick = {
+        () => { props.onOpenArticle('about') } } > About < /a></li >
+    <
+    li > < a href = "javascript:;"
+    onClick = {
+        () => { props.onOpenArticle('contact') } } > Contact < /a></li >
+    <
+    /ul> <
+    /nav> <
+    /header>
 )
 
 Header.propTypes = {
