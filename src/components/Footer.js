cf9def592d8922a7ab8bcc16d1025375ecc3dd37
Philip Orchard <philiporchard@gmail.com>
2018-02-09 20:42:43 -0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Reflective Ventures</p>
+        <p className="copyright">&copy; Reflective Venture Partners</p>
     </footer>
 )
 
