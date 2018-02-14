d970842e99a032ecdb32fc2634696ace22506b9c
Philip Orchard <philiporchard@gmail.com>
2018-02-14 14:02:37 -0800

    update layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,19 @@ import React from 'react'
 const Header = (props) => ( <
     header id = "header"
     style = { props.timeout ? { display: 'none' } : {} } >
-    <
-    div className = "logo" >
-    <
-    span className = "icon fa-connectdevelop" > < /span> < /
-    div > <
-    div className = "content" >
-    <
-    div className = "inner" >
-    <
-    h1 > REFLECTIVE VENTURE PARTNERS < /h1>  <
-    h2 > A development hub
-    for blockchain innovation < /h2>
-    <p>Privately owned and managed venture firm committed to funding and incubating the next generation of core protocols and decentralized solutions</p>
+  <
+  div className = "logo" >
+  <
+  span className = "icon fa-connectdevelop" > < /span> < /
+  div > <
+  div className = "content" >
+  <
+  div className = "inner" >
+  <
+  h1 > REFLECTIVE VENTURE PARTNERS < /h1>  <
+  h2 > A development hub
+  for blockchain innovation < /h2>
+  <p>Privately owned and managed venture firm committed to funding and incubating the next generation of core protocols and decentralized solutions</p>
     </div >
     </div > <
     nav >
@@ -41,10 +41,9 @@ const Header = (props) => ( <
     onClick = {
         () => { props.onOpenArticle('contact') }
     } > Contact < /a></li >
-    <
-    /ul> < /
-    nav > <
-    /header>
+    </ul>
+  </nav > 
+    </header>
 )
 
 Header.propTypes = {
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,111 +7,121 @@ import Main from '../components/Main'
 import Footer from '../components/Footer'
 
 class Template extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isArticleVisible: false,
-      timeout: false,
-      articleTimeout: false,
-      article: '',
-      loading: 'is-loading'
+    constructor(props) {
+        super(props)
+        this.state = {
+            isArticleVisible: false,
+            timeout: false,
+            articleTimeout: false,
+            article: '',
+            loading: 'is-loading'
+        }
+        this.handleOpenArticle = this.handleOpenArticle.bind(this)
+        this.handleCloseArticle = this.handleCloseArticle.bind(this)
     }
-    this.handleOpenArticle = this.handleOpenArticle.bind(this)
-    this.handleCloseArticle = this.handleCloseArticle.bind(this)
-  }
 
-  componentDidMount () {
-    this.timeoutId = setTimeout(() => {
-        this.setState({loading: ''});
-    }, 100);
-  }
-
-  componentWillUnmount () {
-    if (this.timeoutId) {
-        clearTimeout(this.timeoutId);
+    componentDidMount() {
+        this.timeoutId = setTimeout(() => {
+            this.setState({ loading: '' });
+        }, 100);
     }
-  }
 
-  handleOpenArticle(article) {
+    componentWillUnmount() {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+        }
+    }
 
-    this.setState({
-      isArticleVisible: !this.state.isArticleVisible,
-      article
-    })
+    handleOpenArticle(article) {
 
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
+        this.setState({
+            isArticleVisible: !this.state.isArticleVisible,
+            article
+        })
 
-    setTimeout(() => {
-      this.setState({
-        articleTimeout: !this.state.articleTimeout
-      })
-    }, 350)
+        setTimeout(() => {
+            this.setState({
+                timeout: !this.state.timeout
+            })
+        }, 325)
 
-  }
+        setTimeout(() => {
+            this.setState({
+                articleTimeout: !this.state.articleTimeout
+            })
+        }, 350)
 
-  handleCloseArticle() {
+    }
 
-    this.setState({
-      articleTimeout: !this.state.articleTimeout
-    })
+    handleCloseArticle() {
 
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
+        this.setState({
+            articleTimeout: !this.state.articleTimeout
+        })
 
-    setTimeout(() => {
-      this.setState({
-        isArticleVisible: !this.state.isArticleVisible,
-        article: ''
-      })
-    }, 350)
+        setTimeout(() => {
+            this.setState({
+                timeout: !this.state.timeout
+            })
+        }, 325)
 
-  }
+        setTimeout(() => {
+            this.setState({
+                isArticleVisible: !this.state.isArticleVisible,
+                article: ''
+            })
+        }, 350)
 
-  render() {
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const siteDescription = this.props.data.site.siteMetadata.description
+    }
 
-    return (
-      <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
-        <Helmet>
-            <title>{siteTitle}</title>
-            <meta name="description" content={siteDescription} />
-        </Helmet>
+    render() {
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const siteDescription = this.props.data.site.siteMetadata.description
 
-        <div id="wrapper">
+        return ( <
+            div className = { `body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}` } >
+            <
+            Helmet >
+            <
+            title > { siteTitle } < /title> <
+            meta name = "description"
+            content = { siteDescription }
+            /> <
+            /Helmet>
 
-          <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
-          <Main
-            isArticleVisible={this.state.isArticleVisible}
-            timeout={this.state.timeout}
-            articleTimeout={this.state.articleTimeout}
-            article={this.state.article}
-            onCloseArticle={this.handleCloseArticle}
-          />
-          <Footer timeout={this.state.timeout} />
+            <
+            div id = "wrapper" >
 
-        </div>
-        <div id="bg"></div>
+            <
+            Header onOpenArticle = { this.handleOpenArticle }
+            timeout = { this.state.timeout }
+            /> <
+            Main isArticleVisible = { this.state.isArticleVisible }
+            timeout = { this.state.timeout }
+            articleTimeout = { this.state.articleTimeout }
+            article = { this.state.article }
+            onCloseArticle = { this.handleCloseArticle }
+            /> <
+            Footer timeout = { this.state.timeout }
+            />
 
-      </div>
-    )
-  }
+            <
+            /div> <
+            div id = "bg" > < /div>
+
+            <
+            /div>
+        )
+    }
 }
 
 Template.propTypes = {
-  route: React.PropTypes.object,
+    route: React.PropTypes.object,
 }
 
 export default Template
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query PageQuery {
     site {
       siteMetadata {
